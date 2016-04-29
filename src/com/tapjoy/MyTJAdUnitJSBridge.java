0475907a1ac7b124304fe40664995de15445af8d
“yijiajia1988 <yijiajia1988@gmail.com>
2016-04-29 13:12:09 +0800

    commit

--- src/com/tapjoy/MyTJAdUnitJSBridge.java
+++ src/com/tapjoy/MyTJAdUnitJSBridge.java
@@ -5,7 +5,9 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MyTJAdUnitJSBridge extends TJAdUnitJSBridge {
 	private Context context;
@@ -15,7 +17,11 @@ public class MyTJAdUnitJSBridge extends TJAdUnitJSBridge {
 		context = c;
 	}
 
+	@Override
 	public void displayVideo(JSONObject json, String callbackID) {
+		Toast.makeText(context, "displayVideo", Toast.LENGTH_SHORT).show();
+		Log.e("test", "displayVideo json = " + json + "  callbackID = "
+				+ callbackID);
 		String url = "";
 		String cancelMessage = "";
 		try {
@@ -33,6 +39,10 @@ public class MyTJAdUnitJSBridge extends TJAdUnitJSBridge {
 			videoIntent.putExtra("callback_id", callbackID);
 			((Activity) this.context).startActivityForResult(videoIntent, 0);
 		} catch (Exception e) {
+			Toast.makeText(context,
+					"displayVideo Exception e = " + e.toString(),
+					Toast.LENGTH_SHORT).show();
+			Log.e("test", "displayVideo Exception e = " + e.toString());
 			invokeJSCallback(callbackID, new Object[] { Boolean.FALSE });
 			e.printStackTrace();
 		}
